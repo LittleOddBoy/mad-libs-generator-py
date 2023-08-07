91e841a973976edeeb54e5f41827430f18d38c92
LittleOddBoy <hosein.kalari.1387@gmail.com>
2023-08-07 12:38:12 +0330

    print the story and show to user

--- main.py
+++ main.py
@@ -2,3 +2,7 @@
 
 # Save story in a variable and set missing parts of story. (this is A philosophical story, I didn't write it, AI...)
 story = "As the afternoon sun began to cast long shadows, Aristotle sat silently, engrossed in .... contemplation. He realized that Socrates' words had touched upon a .... truth. Happiness, ... desirable, was not the ultimate goal. It was merely a byproduct of a life dedicated to the pursuit of knowledge and .... ."
+
+# Show the story to user.
+print(story)
+
