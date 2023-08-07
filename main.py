c6eaa9396a0898c3444a06f135ea31173dc8459f
LittleOddBoy <hosein.kalari.1387@gmail.com>
2023-08-07 12:42:57 +0330

    feat: get the values of missed points and put them into the story to continue the game.

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ story = "As the afternoon sun began to cast long shadows, Aristotle sat silently
 # Show the story to user.
 print(story)
 
+# Getting the value of missing point from user.
+pOne = input("missing point one: ")
+pTwo = input("missing point two: ")
+pThree = input("missing point three: ")
+pFour = input("missing point four: ")
+
+# And put the user words into the story
+story = f"As the afternoon sun began to cast long shadows, Aristotle sat silently, engrossed in {pOne} contemplation. He realized that Socrates' words had touched upon a {pTwo} truth. Happiness, {pThree} desirable, was not the ultimate goal. It was merely a byproduct of a life dedicated to the pursuit of knowledge and {pFour} ."
